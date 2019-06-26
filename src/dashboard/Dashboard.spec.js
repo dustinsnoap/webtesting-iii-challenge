8a7ee29f0275b0c1f1f30cda25a28100b0e569fd
Dustin Snoap <afrobacon@gmail.com>
2019-06-25 23:13:52 -0400

    MVP

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,13 @@
-// Test away
+import React from 'react'
+import {render, cleanup} from '@testing-library/react'
+import 'jest-dom/extend-expect'
+import Dashboard from './Dashboard'
+afterEach(cleanup)
+
+describe('dashboard', () => {
+    //shows the controls and display
+    it('renders', () => {
+        const dashboard = render(<Dashboard />)
+        expect(dashboard.baseElement).toMatchSnapshot()
+    })
+})
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,62 @@
-// Test away!
+import React from 'react'
+import {render, cleanup} from '@testing-library/react'
+import 'jest-dom/extend-expect'
+afterEach(cleanup)
+
+import Display from './Display'
+
+describe('display', () => {
+    // displays if gate is open/closed and if it is locked/unlocked
+    it('renders', () => {
+        const display = render(<Display />)
+        expect(display.baseElement).toMatchSnapshot()
+    })
+    // displays 'Closed' if the closed prop is true and 'Open' if otherwise
+    it('displays "closed" when closed', () => {
+        const display = render(<Display closed />)
+
+        expect(display.getByText(/closed/i))
+    })
+    it('displays "open" when open', () => {
+        const display = render(<Display closed={false} />)
+
+        expect(display.getByText(/open/i))
+    })
+    // displays 'Locked' if the locked prop is true and 'Unlocked' if otherwise
+    it('displays "unlocked" when unlocked', () => {
+        const display = render(<Display locked />)
+
+        expect(display.getByText(/locked/i))
+    })
+    it('displays "locked" when locked', () => {
+        const display = render(<Display locked={false} />)
+
+        expect(display.getByText(/unlocked/i))
+    })
+    // when unlocked or open use the green-led class
+    it('locked panel sets classname to "green-led" when unlocked', () => {
+        const display = render(<Display locked={false} />)
+        const panel = display.getByTestId(/locked-panel/i)
+
+        expect(panel.classList[1]).toBe('green-led')
+    })
+    it('closed panel sets classname to "green-led" when open', () => {
+        const display = render(<Display closed={false} />)
+        const panel = display.getByTestId(/closed-panel/i)
+
+        expect(panel.classList[1]).toBe('green-led')
+    })
+    // when locked or closed use the red-led class
+    it('locked panel sets classname to "red led" when locked', () => {
+        const display = render(<Display locked />)
+        const panel = display.getByTestId(/locked-panel/i)
+
+        expect(panel.classList[1]).toBe('red-led')
+    })
+    it('closed panel sets classname to "red led" when closed', () => {
+        const display = render(<Display closed />)
+        const panel = display.getByTestId(/closed-panel/i)
+
+        expect(panel.classList[1]).toBe('red-led')
+    })
+})
